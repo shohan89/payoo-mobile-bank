c9c8a9fff0e5c1641c69981bd4e21986235365b5
Sharoar Hossain <maffin.killer007@gmail.com>
2026-02-01 20:20:20 +0600

    Add event listener to login button

---
+++ scripts/login.js
@@ -0,0 +1,8 @@
+// get the login button
+const loginBtn = document.getElementById('LoginBtn');
+
+// add event listener to the login button
+loginBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    console.log('login successfull!!');
+})
