4680e845548445e1cffdc4ac28f1df7269d28846
Sharoar Hossain <maffin.killer007@gmail.com>
2026-02-01 20:44:19 +0600

    Login successfull

--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,14 @@
 // get the login button
 const loginBtn = document.getElementById('LoginBtn');
+const pinInput = document.getElementById('pin');
 
 // add event listener to the login button
 loginBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    console.log('login successfull!!');
+    console.log(typeof pinInput);
+    if ( pinInput === '1234' ) {
+        // window.location.href = '../main.html';
+    } else {
+        alert('Apnar pin thik nei')
+    }
 })
