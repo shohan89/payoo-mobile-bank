b3f693fcada19e4dcd8056122fb7e92955adcefd
Sharoar Hossain <maffin.killer007@gmail.com>
2026-02-02 11:01:38 +0600

    Fix login form logic

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,14 @@
 // get the login button
 const loginBtn = document.getElementById('LoginBtn');
-const pinInput = document.getElementById('pin');
 
 // add event listener to the login button
 loginBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    console.log(typeof pinInput);
-    if ( pinInput === '1234' ) {
-        // window.location.href = '../main.html';
+    const pin = document.getElementById('pin').value;
+    const pinNumber = parseInt(pin);
+
+    if ( pinNumber === 1234 ) {
+        window.location.href = '../main.html';
     } else {
         alert('Apnar pin thik nei')
     }
